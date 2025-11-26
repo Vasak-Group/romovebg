e4d5c37cbd51ea187a975d64533c91be175e2f7c
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2025-11-26 16:33:00 -0300

    refactor: agregar componentes de encabezado y pie de página al diseño principal

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+    <header class="m-4 rounded-xl bg-slate-200 dark:bg-slate-900">
+        <div class="flex h-16 items-center justify-between px-8">
+            <a href="/" class="router-link-active router-link-exact-active flex items-center gap-2" aria-current="page">
+                <span class="text-xl font-bold vsk-text-gradient">
+                    Background Remover
+                </span>
+            </a>
+
+            <nav class="hidden md:flex items-center gap-6">
+                <a href="https://vasak.net.ar/" target="_blank"
+                    class="text-sm font-medium hover:text-primary transition-colors">
+                    Vasak Group
+                </a>
+            </nav>
+        </div>
+    </header>
+</template>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
+import FooterComponent from '../components/FooterComponent.vue';
+import HeaderComponent from '../components/HeaderComponent.vue';
+
 </script>
 
 <template>
+  <HeaderComponent />
   <slot />
+  <FooterComponent />
 </template>
